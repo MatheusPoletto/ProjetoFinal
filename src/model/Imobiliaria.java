e5045dedfd9786c7b0e7213f8386055203b80019
MatheusRibak <matheusribak@hotmail.com>
2015-08-28 08:37:27 -0300

    Parte Ribak

---
+++ src/model/Imobiliaria.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Imobiliaria {
+	
+	// Aqui será feita a classe imboliaria, 
+	// não tem nada porque queria ver se o git ta funcionando depois do fiasco de ontem. 
+
+}
