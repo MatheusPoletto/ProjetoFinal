3a9751bc74e4beb62c619d263cba1fee1d2a4ed5
Matheus Poletto <motaviop@gmail.com>
2015-09-17 02:44:00 -0300

    Commit c/ tlLogin e bar ok.

---
+++ src/tela/telaLogin.java
@@ -0,0 +1,93 @@
+package tela;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+public class telaLogin extends JInternalFrame implements ActionListener{
+
+	private static final long serialVersionUID = 1L;
+	private JTextField jtfUsuario;
+	private JPasswordField jpfSenha;
+	private JLabel jlbUsuario, jlbSenha;
+	private JButton jbtEntrar;
+	private JLabel jlbIcon1;
+
+	telaLogin(){
+		setTitle("Login");
+		setLayout(null);
+		
+		adicionaComponentes();
+		jtfUsuario.setText("Poletto");
+		jpfSenha.setText("123456");
+		setLocation(250, 130);
+		setResizable(false);
+		setSize(400, 300);
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		setVisible(true);
+	}
+
+	private void adicionaComponentes() {
+		jlbUsuario = new JLabel("Nome de usuário", SwingConstants.CENTER);
+		jlbSenha = new JLabel("Senha", SwingConstants.CENTER);
+		jtfUsuario = new JTextField();
+		jpfSenha = new JPasswordField();
+		jbtEntrar = new JButton("ENTRAR");
+		jlbIcon1 = new JLabel();
+
+		jlbUsuario.setBounds(135, 100, 130, 25);
+		jlbSenha.setBounds(135, 160, 130, 25);
+		jtfUsuario.setBounds(125, 130, 150, 25);
+		jpfSenha.setBounds(125, 190, 150, 25);
+		jbtEntrar.setBounds(120, 220, 160, 25);
+		jlbIcon1.setBounds(168, 15, 64, 64);
+
+		jlbUsuario.setFont(new Font("Arial", Font.BOLD, 14));
+		jlbSenha.setFont(new Font("Arial", Font.BOLD, 14));
+		jbtEntrar.addActionListener(this);
+		jbtEntrar.setBackground(Color.GREEN);
+		jlbIcon1.setIcon(new ImageIcon("img/house158.png"));
+
+		jlbUsuario.setVisible(true);
+		jlbSenha.setVisible(true);
+		jtfUsuario.setVisible(true);
+		jpfSenha.setVisible(true);
+		jbtEntrar.setVisible(true);
+		jlbIcon1.setVisible(true);
+
+		getContentPane().add(jlbUsuario);
+		getContentPane().add(jlbSenha);
+		getContentPane().add(jtfUsuario);
+		getContentPane().add(jpfSenha);	
+		getContentPane().add(jbtEntrar);
+		getContentPane().add(jlbIcon1);
+	}
+
+	public static void main(String[] args) {
+		new telaLogin();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == jbtEntrar){
+			if(jtfUsuario.getText().equals("Poletto") && jpfSenha.getText().equals("123456")){
+				this.dispose();
+				JOptionPane.showMessageDialog(null, "Autenticado com sucesso");
+				telaPrincipal.getTlPrincipal().alteraVisibilidade();
+			}else {
+				JOptionPane.showMessageDialog(null, "Falha ao autenticar");
+			}
+		}
+
+	}
+}
---
+++ src/tela/telaPrincipal.java
@@ -0,0 +1,73 @@
+package tela;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class telaPrincipal extends JFrame{
+	private static final long serialVersionUID = 1L;
+	private JMenuBar barra;
+	private telaLogin tlLogin = new telaLogin();
+	private static telaPrincipal tlPrincipal = new telaPrincipal();
+	private JMenu jmnImovel, jmnCadastro, jmnPessoa;
+	private JMenuItem jmiAlugar, jmiVender, jmiCliente, jmiCorretor;
+	
+	telaPrincipal(){		
+		setTitle("M&M Sistema Imobiliário");
+		setLayout(null);
+		
+		getContentPane().add(tlLogin);
+		tlLogin.setVisible(true);
+		
+		barra = new JMenuBar();
+		barra.setVisible(false);
+		
+		jmnCadastro = new JMenu("Cadastro");
+		
+		jmnImovel = new JMenu("Imóvel");
+		jmnCadastro.add(jmnImovel);
+		jmiAlugar = new JMenuItem("Alugar");
+		jmiVender = new JMenuItem("Vender");
+		jmnImovel.add(jmiAlugar);
+		jmnImovel.addSeparator();
+		jmnImovel.add(jmiVender);
+		
+		jmnCadastro.addSeparator();
+		
+		jmnPessoa = new JMenu("Pessoa");
+		jmnCadastro.add(jmnPessoa);
+		jmiCliente = new JMenuItem("Cliente");
+		jmiCorretor = new JMenuItem("Corretor");
+		jmnPessoa.add(jmiCliente);
+		jmnPessoa.addSeparator();
+		jmnPessoa.add(jmiCorretor);
+		
+		barra.add(jmnCadastro);
+		
+		setJMenuBar(barra);
+		setSize(900,600);
+		setVisible(true);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public void alteraVisibilidade(){
+		barra.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		telaPrincipal.getTlPrincipal();
+	}
+
+	public telaLogin getTlLogin() {
+		return tlLogin;
+	}
+
+	public static telaPrincipal getTlPrincipal() {
+		if(tlPrincipal == null){
+			tlPrincipal = new telaPrincipal();
+		}
+		return tlPrincipal;
+	}	
+}
