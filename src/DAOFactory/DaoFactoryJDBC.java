213b1489cef3ae92ced9e0d43e4480b6a5b84dc1
MatheusRibak <matheusribak@hotmail.com>
2015-09-29 13:20:24 -0300

    Arrumando DaoFactoryJDBC (teste)

--- src/DAOFactory/DaoFactoryJDBC.java
+++ src/DAOFactory/DaoFactoryJDBC.java
@@ -1,5 +1,64 @@
 package DAOFactory;
 
-public class DaoFactoryJDBC {
+import dao.ClienteDAO;
+import dao.ClienteDAOJDBC;
+import dao.CorretorDAO;
+import dao.CorretorDAOJDBC;
+import dao.EnderecoDAO;
+import dao.EnderecoDAOJDBC;
+import dao.HistoricoDAO;
+import dao.ImobiliariaDAO;
+import dao.ImobliariaDAOJDBC;
+import dao.ImovelDAO;
+import dao.ImovelDAOJDBC;
+import dao.PessoaDAO;
+import dao.PessoaDAOJDBC;
+
+// tem que digitar como ta se colocar letra minuscula e no dao ta maiscula vai dar erro. 
+
+public class DaoFactoryJDBC implements AbstractDaoFactory {
+
+	@Override
+	public ClienteDAO clienteDAO() {
+	
+		return new ClienteDAOJDBC();
+	}
+
+	@Override
+	public CorretorDAO corretorDAO() {
+	
+		return new CorretorDAOJDBC();
+	}
+
+	@Override
+	public EnderecoDAO enderecoDAO() {
+		
+		return new EnderecoDAOJDBC();
+	}
+
+	@Override
+public HistoricoDAO historicoDAO() {
+	
+		// return new HistoricoDAOJDBC(); //falta fazer historico dao jdbc 
+	return null; 
+	}
+
+	@Override
+	public ImobiliariaDAO imobiliariaDAO() {
+		
+		return new ImobliariaDAOJDBC();  //criado sem o i , não quiz mudar 
+	}
+
+	@Override
+	public ImovelDAO imovelDAO() {
+		
+		return new ImovelDAOJDBC();
+	}
+
+	@Override
+	public PessoaDAO pessoaDAO() {
+		
+		return new PessoaDAOJDBC();
+	}
 
 }
