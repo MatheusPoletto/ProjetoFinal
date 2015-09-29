792235ee14279aac5f6637de85a58f0a83a3bf73
MatheusRibak <matheusribak@hotmail.com>
2015-09-29 09:35:05 -0300

    Arrumando AbstractDaoFactory (Teste)

--- src/DAOFactory/AbstractDaoFactory.java
+++ src/DAOFactory/AbstractDaoFactory.java
@@ -1,5 +1,27 @@
 package DAOFactory;
 
+import dao.ClienteDAO;
+import dao.CorretorDAO;
+import dao.EnderecoDAO;
+import dao.HistoricoDAO;
+import dao.ImobiliariaDAO;
+import dao.ImovelDAO;
+import dao.PessoaDAO;
+
 public interface AbstractDaoFactory {
 
+	// nesta Interface deve ir todos os nossos ....DAO 
+	
+	ClienteDAO clienteDAO(); 
+	CorretorDAO corretorDAO();
+	EnderecoDAO enderecoDAO();
+	HistoricoDAO historicoDAO();
+	ImobiliariaDAO imobiliariaDAO(); 
+	ImovelDAO imovelDAO();
+	PessoaDAO pessoaDAO();
+	
+	
+	
+	
+	
 }
