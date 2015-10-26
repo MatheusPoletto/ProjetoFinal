6d3366bf065a6e346d33b158548ac95b8eb3ea63
Matheus Poletto <motaviop@gmail.com>
2015-10-26 16:26:23 -0200

    Commit com novas alteções no layout da tela.

--- src/tela/telaProcurarPessoa.java
+++ src/tela/telaProcurarPessoa.java
@@ -46,28 +46,108 @@ public class telaProcurarPessoa extends JFrame {
 	private DefaultTableModel dtbPessoas;
 	private JScrollPane jspPessoas;
 	private JToolBar jtbBarra;
-	private Boolean barraVisivel = false;
+	private Boolean menuVisivel = false;
 	private JLabel jlbTitulo;
 	private CorretorDAO corretorDao = DaoFactoryJDBC.get().corretorDAO();
 	private ClienteDAO clienteDao = DaoFactoryJDBC.get().clienteDAO();
 
 	public telaProcurarPessoa() {
-		setTitle("");
+		setTitle("Registro de pessoas");
 		setLayout(null);
 
-		jlbNome = new JLabel("Nome:");
+		criarLabelTitulo();
+		criarTabela();
+		criarMenu();
+		
+		cliqueBotao();
 
-		jlbRG = new JLabel("RG");
+		setResizable(false);
+		setSize(707, 500);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	private void cliqueBotao() {
+		//Abrir barra com menu
+		jbtMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(menuVisivel == false){
+					jspPessoas.setBounds(110, 45, 590, 500);
+					jtbBarra.setVisible(true);
+					menuVisivel = true;
+				}else {
+					jspPessoas.setBounds(0, 45, 702, 500);
+					jtbBarra.setVisible(false);
+					menuVisivel = false;
+				}
+			}
+		});
+		
+	}
 
-		jlbCpf = new JLabel("CPF");
+	private void criarMenu() {
+		jbtMenu = new JButton();
+		jbtMenu.setBounds(0, 0, 110, 44);
+		jbtMenu.setVisible(true);
+		jbtMenu.setIcon(new ImageIcon("botoes_png/menu.png"));
+		getContentPane().add(jbtMenu);
+		
+		jbtCriarPessoa = new JButton("NOVO");
+		jbtEditarPessoa = new JButton("EDITAR");
+		jbtRemoverPessoa = new JButton("REMOVER");
+		jbtAtualizarPessoas = new JButton("ATUALIZAR");
+		
+		jbtCriarPessoa.setBounds(0,0,110,30);
+		jbtEditarPessoa.setBounds(0,90,110,30);
+		jbtRemoverPessoa.setBounds(0,30,110,30);
+		jbtAtualizarPessoas.setBounds(0,60,110,30);
 
-		jlbNome.setBounds(0, 0, 50, 30);
-		jlbRG.setBounds(50, 0, 50, 30);
-		jlbCpf.setBounds(100, 0, 50, 30);
+		jtbBarra = new JToolBar();
+		jtbBarra.setBackground(Color.white);
+		jtbBarra.setOrientation(1);
+		jtbBarra.setFloatable(false);
+		jtbBarra.setLayout(null);
+		jtbBarra.setBounds(0, 44, 110, 500);
+		jtbBarra.setVisible(false);
+		jtbBarra.add(jbtCriarPessoa);
+		jtbBarra.add(jbtEditarPessoa);
+		jtbBarra.add(jbtRemoverPessoa);
+		jtbBarra.add(jbtAtualizarPessoas);
+		getContentPane().add(jtbBarra);
+		
+	}
 
+	private void criarTabela() {
+		jtbPessoas = new JTable();
+		getContentPane().add(jtbPessoas);
+		dtbPessoas = new DefaultTableModel();
+		
+		dtbPessoas.addColumn("ID");
+		dtbPessoas.addColumn("NOME");
+		dtbPessoas.addColumn("RG");
+		dtbPessoas.addColumn("CPF");
+		dtbPessoas.addColumn("TIPO");
+		
+		jtbPessoas.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		jtbPessoas.setModel(dtbPessoas);
+		jspPessoas = new JScrollPane(jtbPessoas);
+		jspPessoas.setBounds(0, 45, 702, 500);
+		jspPessoas.setVisible(true);
+		getContentPane().add(jspPessoas);
+		
+		jtbPessoas.getColumnModel().getColumn(0).setPreferredWidth(46);
+		jtbPessoas.getColumnModel().getColumn(1).setPreferredWidth(210);
+		jtbPessoas.getColumnModel().getColumn(2).setPreferredWidth(80);
+		jtbPessoas.getColumnModel().getColumn(3).setPreferredWidth(100);
+		jtbPessoas.getColumnModel().getColumn(4).setPreferredWidth(100);
+		
+		alimentarTabela();
+		
+	}
+	
+	private void criarLabelTitulo() {
 		jlbTitulo = new JLabel("REGISTRO DE PESSOAS", SwingConstants.CENTER);
 		jlbTitulo.setBounds(110, 0, 595, 44);
-		//jlbTitulo.setIcon(new ImageIcon("img/titulo_registropessoas.png"));
 		jlbTitulo.setVisible(true);
 		jlbTitulo.setFont(new Font("ARIAL", Font.PLAIN, 18));
 		jlbTitulo.setOpaque(true);
@@ -75,106 +155,24 @@ public class telaProcurarPessoa extends JFrame {
 		jlbTitulo.setForeground(Color.white);
 		getContentPane().add(jlbTitulo);
 		
-		jpnFiltro = new JPanel();
-		jpnFiltro.setBounds(200, 3, 400, 40);
-		jpnFiltro.setVisible(false);
-		jpnFiltro.setLayout(null);
-		jpnFiltro.add(jlbNome);
-		jpnFiltro.add(jlbRG);
-		jpnFiltro.add(jlbCpf);
-		jpnFiltro.setBorder(BorderFactory.createTitledBorder(""));
-		getContentPane().add(jpnFiltro);
-
-		jtbPessoas = new JTable();
-		getContentPane().add(jtbPessoas);
-		dtbPessoas = new DefaultTableModel();
-		dtbPessoas.addColumn("ID");
-		dtbPessoas.addColumn("Nome");
-		dtbPessoas.addColumn("RG");
-		dtbPessoas.addColumn("CPF");
-		alimentarDtb();
-		jtbPessoas.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		jtbPessoas.setModel(dtbPessoas);
-		jspPessoas = new JScrollPane(jtbPessoas);
-		jspPessoas.setBounds(0, 45, 702, 500);
-		//jspPessoas.setBounds(65, 0, 736, 500);
-		jspPessoas.setVisible(true);
-		getContentPane().add(jspPessoas);
-		jtbPessoas.getColumnModel().getColumn(0).setPreferredWidth(46);
-		jtbPessoas.getColumnModel().getColumn(1).setPreferredWidth(310);
-		jtbPessoas.getColumnModel().getColumn(2).setPreferredWidth(80);
-		jtbPessoas.getColumnModel().getColumn(3).setPreferredWidth(100);
-
-
-		jbtEditarPessoa = new JButton("EDITAR");
-
-		jbtRemoverPessoa = new JButton("REMOVER");
-
-		jbtCriarPessoa = new JButton("NOVO");
-		
-		jbtAtualizarPessoas = new JButton("ATUALIZAR");
-
-	
-		jbtMenu = new JButton();
-		jbtMenu.setBounds(0, 0, 110, 44);
-		jbtMenu.setVisible(true);
-		jbtMenu.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				if(barraVisivel == false){
-					jspPessoas.setBounds(100, 45, 600, 500);
-					jtbBarra.setVisible(true);
-					barraVisivel = true;
-				}else {
-					jspPessoas.setBounds(0, 45, 702, 500);
-					jtbBarra.setVisible(false);
-					barraVisivel = false;
-				}
-				
-			}
-		});
-		jbtMenu.setIcon(new ImageIcon("botoes_png/menu.png"));
-		getContentPane().add(jbtMenu);
-
-		jtbBarra = new JToolBar();
-		jtbBarra.setBackground(Color.white);
-		jtbBarra.setOrientation(1);
-		jtbBarra.setFloatable(false);
-		jtbBarra.setLayout(null);
-		jtbBarra.setBounds(0, 44, 100, 500);
-		jtbBarra.setVisible(false);
-		//jtbBarra.setBounds(0, 0, 65, 500);
-		jtbBarra.add(jbtCriarPessoa);
-		jbtCriarPessoa.setBounds(0,0,100,30);
-		jtbBarra.add(jbtRemoverPessoa);
-		jbtRemoverPessoa.setBounds(0,30,100,30);
-		jtbBarra.add(jbtAtualizarPessoas);
-		jbtAtualizarPessoas.setBounds(0,60,100,30);
-		jtbBarra.add(jbtEditarPessoa);
-		jbtEditarPessoa.setBounds(0,90,100,30);
-		//jtbBarra.addSeparator();
-		jtbBarra.add(jbtAtualizarPessoas);
-		getContentPane().add(jtbBarra);
-
-		setResizable(false);
-		setSize(707, 500);
-		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
+	
 
-	private void alimentarDtb() {
+	private void alimentarTabela() {
 		for(Corretor corretor: corretorDao.todos()){
-			dtbPessoas.addRow(new String[] {String.valueOf(corretor.getIdCorretor()), corretor.getPessoa().getNome(), corretor.getPessoa().getRg(), corretor.getPessoa().getCpf()});
+			dtbPessoas.addRow(new String[] {String.valueOf(corretor.getIdCorretor()), corretor.getPessoa().getNome(), corretor.getPessoa().getRg(), corretor.getPessoa().getCpf() , "CORRETOR"});
 		}
 		
 		for(Cliente cliente: clienteDao.todos()){
-			dtbPessoas.addRow(new String[]{String.valueOf(cliente.getIdCliente()), cliente.getPessoa().getNome(), cliente.getPessoa().getRg(), cliente.getPessoa().getCpf()});
+			dtbPessoas.addRow(new String[]{String.valueOf(cliente.getIdCliente()), cliente.getPessoa().getNome(), cliente.getPessoa().getRg(), cliente.getPessoa().getCpf() , "CLIENTE"});
 		}
-		
+	
 	}
+	
 
 	public static void main(String[] args) {
 		new telaProcurarPessoa();
+		
 	}
+	
 }
