db3a0aee12b2974c55c5ea723106b3972a454a26
MatheusRibak <matheusribak@hotmail.com>
2015-11-19 20:43:06 -0200

    Ajuda

---
+++ src/dao/AjudaDAO.java
@@ -0,0 +1,7 @@
+package dao;
+
+import model.Ajuda;
+
+public interface AjudaDAO extends GenericDAO<Ajuda> {
+
+}
---
+++ src/dao/AjudaDAOJDBC.java
@@ -0,0 +1,17 @@
+package dao;
+
+import java.sql.Connection;
+
+import conexao.ConexaoUtil;
+
+public class AjudaDAOJDBC {
+	
+	private Connection con;
+
+	public  AjudaDAOJDBC(){
+		con = ConexaoUtil.getCon();
+	}
+
+	
+	
+}
