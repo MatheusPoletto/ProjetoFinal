83ca60ad94a667589b86d05a695ec0bf149d8f0f
Matheus Poletto <motaviop@gmail.com>
2015-12-02 14:59:21 -0200

    Update AlterarNota.java

--- src/metodos/AlterarNota.java
+++ src/metodos/AlterarNota.java
@@ -2,5 +2,12 @@ package metodos;
 
 public class AlterarNota {
 
+//CORRIGIR!
+  private NotaFiscalDAO nfDao = DAOFACTORYJDBC.get().notaFiscalDAO;
+
+  public excluirNota(NotaFiscal nf, Venda venda){
+  	nfDao.excluir(nf);
+	  vendaDao.excluir(venda);
+  }
 
 }
